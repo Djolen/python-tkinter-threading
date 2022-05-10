3c778d6de577b8ff77ebe4881715a54638a89a46
Djolen <djordjert5419@gs.viser.edu.rs>
2022-05-11 00:26:55 +0200

    popravljeno ugasi dugme

--- client.py
+++ client.py
@@ -5,12 +5,25 @@ import socket
 import time
 
 
-def posaljiNaServer():
+class Trigger:
+    def __init__(self):
+        self.trigger = False
 
+    def setTrigger(self):
+        self.trigger = True
+
+    def getTrigger(self):
+        return self.trigger
+
+
+newTrigger = Trigger()
+
+
+def posaljiNaServer():
     s = socket.socket()
     host = socket.gethostname()
     port = 12345
-    s.connect((host,port))
+    s.connect((host, port))
     ime = unosIme.get()
     x = var1.get() + ime
 
@@ -18,13 +31,15 @@ def posaljiNaServer():
     odgovor = (s.recv(1024).decode())
     if odgovor != "UGASI":
         var = StringVar()
-        label = Label(prozorClient, textvariable=var, relief=GROOVE, font=("Arial",18))
+        label = Label(prozorClient, textvariable=var, relief=GROOVE, font=("Arial", 18))
         var.set(odgovor)
         label.pack()
     else:
         print("ugasi")
+        newTrigger.setTrigger()
 
     s.close()
+
 
 prozorClient = Tk()
 
@@ -33,7 +48,7 @@ posaljiDugme = Button(prozorClient, text="Posalji", command=posaljiNaServer)
 posaljiDugme.pack()
 
 f = ("Arial", 19)
-var1 = StringVar()
+var1 = StringVar(value="1")
 
 labelaIme = Label(prozorClient, text="Ime: ", font=f)
 labelaIme.pack()
@@ -45,21 +60,34 @@ radioUgasi = Radiobutton(prozorClient, text="Ugasi", value="2", variable=var1)
 radioVreme.pack()
 radioUgasi.pack()
 
+
 def arkTred():
     extent = 1
     extent2 = 1
     while True and extent2 != -180:
-        mojCanvas.delete(ALL)
-        prviKrug = mojCanvas.create_arc((150, 150, 250, 250), start=0, extent=180, fill="red", outline="blue")
-        mojCanvas.create_arc((150, 150, 250, 250), start=extent, extent=0)
-        #drugiKrug = mojCanvas.create_arc((150, 150, 250, 250), start=180, extent=extent2, fill="blue", outline="blue")
-        extent += 1
-        #extent2 -= 1
-        if extent == 180:
-            extent = 0
-        time.sleep(0.00002)
+        if newTrigger.getTrigger():
+            mojCanvas.delete(ALL)
+            mojCanvas.create_arc((150, 150, 250, 250), start=0, extent=180, fill="red", outline="blue")
+            # mojCanvas.create_arc((150, 150, 250, 250), start=extent, extent=0)
+            mojCanvas.create_arc((150, 150, 250, 250), start=180, extent=extent2, fill="blue", outline="blue")
+            # extent += 1
+            extent2 -= 1
+            # if extent == 180:
+            #    extent = 0
+            time.sleep(0.00002)
+        else:
+            mojCanvas.delete(ALL)
+            mojCanvas.create_arc((150, 150, 250, 250), start=0, extent=180, fill="red", outline="blue")
+            mojCanvas.create_arc((150, 150, 250, 250), start=extent, extent=0)
+            # mojCanvas.create_arc((150, 150, 250, 250), start=180, extent=extent2, fill="blue", outline="blue")
+            extent += 1
+            # extent2 -= 1
+            if extent == 180:
+                extent = 0
+            time.sleep(0.00002)
 
-mojCanvas = Canvas(prozorClient, bg ="blue", width=400, height=250)
+
+mojCanvas = Canvas(prozorClient, bg="blue", width=400, height=250)
 mojCanvas.pack(anchor=E)
 threading.Thread(target=arkTred).start()
 prozorClient.mainloop()
